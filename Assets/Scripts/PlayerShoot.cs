c6f5f3e2e07023a2c5e22c2703a598f2dbbe7b33
Tom Hewitt <tommhewitt@gmail.com>
2019-01-25 20:03:48 +0000

    Added ability to shoot

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+	public GameObject bullet;
+
+	public Transform spawn;
+
+	public float fireRate;
+	public float bulletForce;
+
+	private float cooldown;
+
+	private void Update()
+	{
+		cooldown -= Time.deltaTime;
+
+		if (Input.GetAxis("RightTrigger") >= 0.95f && cooldown <= 0)
+		{
+			Shoot();
+		}
+	}
+
+	private void Shoot()
+	{
+		cooldown = fireRate;
+
+		GameObject b = Instantiate(bullet, spawn.position, spawn.rotation) as GameObject;
+		b.GetComponent<Rigidbody>().AddForce(b.transform.forward * bulletForce, ForceMode.Impulse);
+	}
+}
