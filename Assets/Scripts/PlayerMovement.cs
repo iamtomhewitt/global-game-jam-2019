71f3d5c5e1836f0ec0b5febef40a77604bd2df15
Tom Hewitt <tommhewitt@gmail.com>
2019-01-25 18:52:34 +0000

    Added player model and simple move script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float moveSpeed;
+
+	public KeyCode forwardKey, leftKey, rightKey, backwardKey;
+
+	private void FixedUpdate()
+	{
+		if (Input.GetKey(forwardKey))
+		{
+			transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(backwardKey))
+		{
+			transform.position -= transform.forward * moveSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(leftKey))
+		{
+			transform.position -= transform.right * moveSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(rightKey))
+		{
+			transform.position += transform.right * moveSpeed * Time.deltaTime;
+		}
+	}
+}
