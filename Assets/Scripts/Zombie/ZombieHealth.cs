edd41e998ab408a6a289456ca469b118d2fb9096
Tom Hewitt <tommhewitt@gmail.com>
2019-01-26 00:03:35 +0000

    Added Zombie movement and started Zombie health script

---
+++ Assets/Scripts/Zombie/ZombieHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHealth : MonoBehaviour
+{
+	public int health = 100;
+
+	public void DecreaseHealth(int amount)
+	{
+		health -= amount;
+
+		if (health <= 0)
+		{
+			print("Zombie is DEAD!");
+			Destroy(this.gameObject);
+		}
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		switch (other.gameObject.tag)
+		{
+			case "Bullet":
+				DecreaseHealth(5);
+				break;
+
+			default:
+				Debug.LogError("Unrecognised tag: " + other.gameObject.name);
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/Zombie/ZombieMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieMovement : MonoBehaviour
+{
+	private NavMeshAgent agent;
+
+	private void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(GameObject.Find("Zombie Destination").transform.position);
+	}
+}
