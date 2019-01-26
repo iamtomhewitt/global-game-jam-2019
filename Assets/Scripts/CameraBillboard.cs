b80abc9fee850fbe1910110e602ac0fc098eabc4
Tom Hewitt <tommhewitt@gmail.com>
2019-01-26 10:31:15 +0000

    Updated weapon
    - Shotgun has a kick back when fired
    - Shotgun shells come out of the gun

---
+++ Assets/Scripts/CameraBillboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBillboard : MonoBehaviour
+{
+	public Vector3 rotation = new Vector3(0f, 45f, 0f);
+
+	private void Update()
+	{
+		transform.rotation = Quaternion.Euler(rotation);
+	}
+}
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour
 {
 	public GameObject bullet;
+	public GameObject shotgunShell;
 
 	public Transform spawn;
+	public Transform shotgunShellSpawn;
 
 	public float fireRate;
 	public float bulletForce;
+	public float shotgunShellForce;
+	public float recoil;
 
 	private float cooldown;
+
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
 	private void Update()
 	{
@@ -29,5 +40,12 @@ public class PlayerShoot : MonoBehaviour
 
 		GameObject b = Instantiate(bullet, spawn.position, spawn.rotation) as GameObject;
 		b.GetComponent<Rigidbody>().AddForce(b.transform.forward * bulletForce, ForceMode.Impulse);
+
+		GameObject s = Instantiate(shotgunShell, shotgunShellSpawn.position, shotgunShellSpawn.rotation) as GameObject;
+		s.GetComponent<Rigidbody>().AddForce(s.transform.right * shotgunShellForce, ForceMode.Impulse);
+		s.GetComponent<Rigidbody>().AddTorque(Random.insideUnitSphere * Random.Range(0, shotgunShellForce*2));
+		Destroy(s.gameObject, 5f);
+
+		rb.AddForce(-transform.forward * recoil, ForceMode.Impulse);
 	}
 }
